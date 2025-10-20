eabaf1e5d18e9401b7f3e741a0aa0480d2502b00
Ccccred <3565530270@qq.com>
2025-10-20 23:54:16 +0800

    Update 1_main.c
    Updated the program to convert Celsius to Fahrenheit and added user input.

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
-#include <stdio.h>
+//202510304103
+//3565530270@qq.com
+//刘栩池
+#include<stdio.h>
 int main()
 {
-    printf("Hello World");
+    float a;
+    printf("请输入摄氏温度：");
+    scanf("%f",&a);
+    float b=a*9/5+32;
+    printf("%.1f",b);
     return 0;
 }
