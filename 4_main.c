c45432693e54f7d26faa8536b66a66ac9d9569f3
Ccccred <3565530270@qq.com>
2025-10-22 20:40:42 +0800

    Update 4_main.c

--- 4_main.c
+++ 4_main.c
@@ -7,8 +7,8 @@
     printf("请输入三角形的三边长(整数)：");
     scanf("%d %d %d",&a,&b,&c);
     if(a+b>c && a+c>b && b+c>a)
-    printf("能构成三角形");
+    printf("可以组成三角形");
     else
-    {printf("不能构成三角形");}
+        printf("不能组成三角形");
         return 0;
  }
